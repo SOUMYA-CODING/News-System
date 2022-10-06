6404de506cbb7c26ce62e5e5a73ba66092984414
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-10-06 14:52:51 +0530

    update

---
+++ frontend/app/src/main/java/com/example/news_app/authentications_screen/WelcomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.news_app.authentications_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.news_app.R;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
