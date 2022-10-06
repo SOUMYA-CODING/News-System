1e9939c60dd918fe1d33557212b78af34d29cd0a
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-10-06 15:11:51 +0530

    update

---
+++ frontend/app/src/main/java/com/example/newsapp/home_page/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.newsapp.home_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newsapp.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
