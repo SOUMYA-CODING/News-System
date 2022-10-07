e2e1888f96ac3390b39457fdda69a9beb6cd5165
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-10-07 18:02:58 +0530

    Update

---
+++ frontend/app/src/main/java/com/example/newsapp/onboarding/OnboardingScreen.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsapp.R
+
+class OnboardingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen)
+    }
+}
---
+++ frontend/app/src/main/java/com/example/newsapp/welcome/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsapp.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
