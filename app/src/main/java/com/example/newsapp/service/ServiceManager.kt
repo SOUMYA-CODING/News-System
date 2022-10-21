b8a2b7c237399627451fc898a256cec770d4435c
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-10-22 00:34:04 +0530

    App Update

--- app/src/main/java/com/example/newsapp/service/ServiceManager.kt
+++ app/src/main/java/com/example/newsapp/service/ServiceManager.kt
@@ -1,4 +1,23 @@
 package com.example.newsapp.service
 
+// Retrofit Package
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
+import retrofit2.http.Url;
+
 class ServiceManager {
+    val apiURL = "https://soumyasahu.live/"
+
 }
